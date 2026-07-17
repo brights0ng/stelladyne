9f024c678265aa7d75f4ab2f4bdc0bcb2092e845
sethcolson <sdcolson1@gmail.com>
2026-07-17 18:14:48 -0500

    Cleaned up unwanted git files, did some package organizing.

--- cli/src/main/java/dev/brights0ng/stelladyne/cli/GenerateCommand.java
+++ cli/src/main/java/dev/brights0ng/stelladyne/cli/GenerateCommand.java
@@ -2,6 +2,7 @@ package dev.brights0ng.stelladyne.cli;
 
 import dev.brights0ng.stelladyne.core.pipeline.FormationPipeline;
 import dev.brights0ng.stelladyne.core.result.StarSystem;
+import dev.brights0ng.stelladyne.util.SeedGenerator;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -11,10 +12,17 @@ import java.util.concurrent.Callable;
 public class GenerateCommand implements Callable<Integer> {
 
     @Option(names = "--seed", required = true)
-        long seed;
+        String seedInput;
 
     @Override
     public Integer call() throws Exception {
+        long seed = 0; // fallback
+
+        try {
+            seed = Long.parseLong(seedInput);
+        } catch (NumberFormatException e) {
+            seed = SeedGenerator.stringToSeed(seedInput);
+        }
         StarSystem system = new FormationPipeline().run(seed);
         System.out.println(system);
         return 0;
--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module dev.brights0ng.stelladyne.core {
     exports dev.brights0ng.stelladyne.core.pipeline;
     exports dev.brights0ng.stelladyne.core.result;
+    exports dev.brights0ng.stelladyne.util;
 }
